c1bf1e709078b972eeb1232ef7e26d2154d4c761
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-01-19 16:48:53 -0500

    add woff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,10 @@
-import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
 
 const hibiscus = localFont({
-    src: "../../public/fonts/HibiscusSans-Regular.otf",
+    src: "../../public/fonts/HibiscusSans-Regular.woff",
     variable: "--font-hibiscus",
 });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-export const metadata: Metadata = {
-  title: "Hibiscus Sans",
-  description: "a sans serif font",
-};
 
 export default function RootLayout({
   children,
